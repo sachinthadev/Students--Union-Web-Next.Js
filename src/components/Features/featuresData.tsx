2b723ead5120cab84ad5e952934777c6937a711b
sachinthadev <sahansachintha.dev@gmail.com>
2024-12-17 11:35:28 +0530

    Fix Images issue

--- src/components/Features/featuresData.tsx
+++ src/components/Features/featuresData.tsx
@@ -6,25 +6,24 @@ const featuresData: Feature[] = [
     id: 1,
     icon: (
       <Image  
-        src="./images/news/newse1.png" 
+        src="/images/news/newse1.png" 
         alt="Description of the photo" 
-        width="300" 
-        height="300" 
+        width={300} 
+        height={300} 
       />
     ),
     title: "New President of IHNDSF",
     paragraph:
-      "Chamalka Dilshan has been Selected as the new President of Inter higher national diploma students fedaration. 10/09/2024",
+      "Chamalka Dilshan has been Selected as the new President of Inter higher national diploma students federation. 10/09/2024",
   },
-  
   {
-    id: 1,
+    id: 2,
     icon: (
       <Image  
-        src="./images/news/newse2.png" 
+        src="/images/news/newse2.png" 
         alt="Description of the photo" 
-        width="300" 
-        height="300" 
+        width={300} 
+        height={300} 
       />
     ),
     title: "Newly appointed Committee",
@@ -32,13 +31,13 @@ const featuresData: Feature[] = [
       "These are the newly appointed Committee members of Student's union- Advanced Technological Institute - Kurunegala. 10/21/2024",
   },
   {
-    id: 1,
+    id: 3,
     icon: (
       <Image  
-        src="./images/news/newse3.png" 
+        src="/images/news/newse3.png" 
         alt="Description of the photo" 
-        width="300" 
-        height="300" 
+        width={300} 
+        height={300} 
       />
     ),
     title: "Student Hero's day",
@@ -46,13 +45,13 @@ const featuresData: Feature[] = [
       "They Have loved you unconditionally, Student Hero's day. 06/01/2024",
   },
   {
-    id: 1,
+    id: 4,
     icon: (
       <Image  
-        src="./images/news/newse4.png" 
+        src="/images/news/newse4.png" 
         alt="Description of the photo" 
-        width="300" 
-        height="300" 
+        width={300} 
+        height={300} 
       />
     ),
     title: "No Top-ups!",
@@ -60,27 +59,27 @@ const featuresData: Feature[] = [
       "No Top-ups! turn SLIATE as an Institution award Degrees 05/31/2024",
   },
   {
-    id: 1,
+    id: 5,
     icon: (
       <Image  
-        src="./images/news/newse5.png" 
+        src="/images/news/newse5.png" 
         alt="Description of the photo" 
-        width="300" 
-        height="300" 
+        width={300} 
+        height={300} 
       />
     ),
     title: "New Intake 2024",
     paragraph:
-      "We are happy to announce you that  HND New Gazette for  2023/2024 had been published. Let's Join with us to make your goals a Success. 04/27/2024",
+      "We are happy to announce you that HND New Gazette for 2023/2024 had been published. Let's Join with us to make your goals a Success. 04/27/2024",
   },
   {
-    id: 1,
+    id: 6,
     icon: (
       <Image  
-        src="./images/news/newse6.png"   
+        src="/images/news/newse6.png"   
         alt="Description of the photo" 
-        width="300" 
-        height="300" 
+        width={300} 
+        height={300} 
       />
     ),
     title: "19th Annual Convocation of SLIATE 2024",
@@ -88,4 +87,5 @@ const featuresData: Feature[] = [
       "19th Annual Convocation of SLIATE 2024 Congratulations to all the brothers and sisters on your success that you really earned. May 06,07,08 at BMICH 05/06/2024",
   },
 ];
+
 export default featuresData;
